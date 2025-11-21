6866271db6263e99b23822f944a14e86a8dfddb5
Monica Nieckula <108962651+emresenDEV@users.noreply.github.com>
2025-11-21 09:55:44 -0600

    disable '/' trailing api calls

--- backend/app.py
+++ backend/app.py
@@ -27,6 +27,7 @@ from routes.services_route import service_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 
 # Configure ProxyFix to trust Cloudflare Tunnel headers
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
