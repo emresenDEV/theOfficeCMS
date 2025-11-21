2b02ef4217712034aa27a076ede835034f4284e0
Monica Nieckula <108962651+emresenDEV@users.noreply.github.com>
2025-11-21 14:48:24 -0600

    Flask update to IPv4 instead of IPv6

--- backend/app.py
+++ backend/app.py
@@ -84,4 +84,4 @@ def test_cors():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host="::", port=5002, debug=False)
+    app.run(host="0.0.0.0", port=5002, debug=False)
